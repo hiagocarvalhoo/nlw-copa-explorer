4a5938ca07f1ea4938cb964cff1f7459a465e200
Hiago Carvalho <hiaagocarvalho@gmail.com>
2023-01-15 20:10:04 -0300

    leaving the game card dynamics

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-function createGame() {
+function createGame(player1, hour, player2) {
   return `
   <li>
-    <img src="./assets/icon-brazil.svg" alt="Bandeira do Brasil">
-    <strong>16:00</strong>
-    <img src="./assets/icon-serbia.svg" alt="Bandeira da Sérvia">
+    <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
+    <strong>${hour}</strong>
+    <img src="./assets/icon-${player2}.svg" alt="Bandeira da ${player2}">
   </li>
   `
 }
 
-function createCard (date, day) {
+function createCard (date, day, games) {
   return `
     <div class="card">
       <h2>${date} <span>${day}</span></h2>
       <ul>
-        
+        ${games}
       </ul>
     </div>
   `
@@ -24,8 +24,8 @@ document.querySelector("#app").innerHTML = `
     <img src="./assets/logo.svg" alt="Logo da NLW Copa">
   </header>
   <main id="cards">
-    ${createCard("24/11", "quinta")}
-    ${createCard("28/11", "segunda")}
-    ${createCard("02/12", "sexta")}
+    ${createCard("24/11", "quinta", createGame("brazil", "16:00", "serbia"))}
+    ${createCard("28/11", "segunda", createGame("brazil", "16:00", "serbia"))}
+    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "serbia"))}
   </main>
 `
