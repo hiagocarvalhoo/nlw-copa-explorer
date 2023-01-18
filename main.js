1645b7b52aacc173c0c7bd88e37e1066c0dcc7f4
Hiago Carvalho <hiaagocarvalho@gmail.com>
2023-01-17 23:08:26 -0300

    solving bug

--- main.js
+++ main.js
@@ -19,8 +19,14 @@ function createCard (date, day, games) {
   `
 }
 
-document.querySelector("#app").innerHTML = `
-    ${createCard("24/11", "quinta", createGame("switzerland", "07:00", "cameroon") + createGame("uruguay", "10:00", "southkorea") + createGame("portugal", "13:00", "ghana") + createGame("brazil", "16:00", "serbia"))}
-    ${createCard("28/11", "segunda", createGame("switzerland", "13:00", "brazil"))}
-    ${createCard("02/12", "sexta", createGame("brazil", "16:00", "cameroon"))}
-`
+document.querySelector("#cards").innerHTML = 
+  createCard("24/11", "quinta", createGame("brazil", "16:00",
+  "serbia")) +
+  createCard(
+    "28/11",
+    "segunda",
+    createGame("switzerland", "13:00", "brazil") +
+      createGame("portugal", "16:00", "uruguay")
+  ) +
+  createCard("02/12", "sexta", createGame("brazil", "16:00",
+  "cameroon"))
